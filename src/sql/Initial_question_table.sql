f481a3b54139f8fe8b8412b92bc16734dd3b65d3
Haoran Li <aimee@dn0a229345.sunet>
2013-12-01 23:38:38 -0800

    database changed

--- src/sql/Initial_question_table.sql
+++ src/sql/Initial_question_table.sql
@@ -30,7 +30,7 @@ CREATE TABLE QR (
 );
 INSERT INTO QR VALUES
  (0, "Siqi", "What is the teacher's name? (CS108)", "Patrick Young", 
- 		 5, "#CommonSense##CS#", 0);
+ 		 5, "CommonSense#CS", 0);
 
 -- ======================================================================	
 -- Question Type Two Fill In The Blank(FB)
@@ -49,7 +49,7 @@ CREATE TABLE FB (
 
 INSERT INTO FB VALUES
 (0, "Siqi", "#blank# is the CS108's teacher this quarter(2013Autumn).", "Patrick Young", 
-		5, "#CommonSense##CS#", 0);
+		5, "CommonSense#CS", 0);
 
 -- ======================================================================	
 -- Question Type Three Multiple Choice(MC)
@@ -69,7 +69,7 @@ CREATE TABLE MC (
          
 INSERT INTO MC VALUES
 (0, "Siqi", "What is the teacher's name? (CS108)", 
-		"#Zhang##Li##Du##Cheng#Patrick Young#","Patrick Young",5, "#CommonSense##CS#", 0);
+		"Zhang#Li#Du#Cheng#Patrick Young","Patrick Young",5, "CommonSense#CS", 0);
 	 
 -- ======================================================================	
 -- Question Type Four Picture Response(PR)
@@ -89,7 +89,7 @@ CREATE TABLE PR (
          
 INSERT INTO PR VALUES
 (0,"Siqi",  "What is the Acronym of this building ?","MC",
-		"http://events.stanford.edu/events/252/25201/Memchu_small.jpg", 5, "#CommonSense##Stanford#", 0);  
+		"http://events.stanford.edu/events/252/25201/Memchu_small.jpg", 5, "CommonSense#Stanford", 0);  
 		
 -- ======================================================================	
 -- Question Type Five Multiple Answer(MA)
@@ -108,8 +108,8 @@ CREATE TABLE MA (
 );
 
 INSERT INTO MA VALUES        
-(0,"Siqi",  "List the last name of all the team members in this project group?","#Zhang##Li##Du##Cheng#",
-		"false", 8, "#CommonSense##Stanford#", 0);  
+(0,"Siqi",  "List the last name of all the team members in this project group?","Zhang#Li#Du#Cheng",
+		"false", 8, "CommonSense#Stanford", 0);  
 
 -- ======================================================================	
 -- Question Type Six Multiple Choice Multiple Answer(MCMA)
@@ -127,8 +127,8 @@ CREATE TABLE MCMA (
     Time INT    
 );
 INSERT INTO MCMA VALUES
-(0,"Siqi", "What is the last name of all the team members in this project group?(Choose all that is correct)","#Zhang##Li##Du##Cheng#",
-		"#Zhang##Li##Du##Cheng#", 8, "#CommonSense##Stanford#", 0); 
+(0,"Siqi", "What is the last name of all the team members in this project group?(Choose all that is correct)","Zhang#Li#Du#Cheng",
+		"Zhang#Li#Du#Cheng", 8, "CommonSense#Stanford", 0); 
 
 -- ======================================================================	
 -- Question Type Seven Matching Question (MQ)
@@ -147,5 +147,5 @@ CREATE TABLE MQ (
 );
 
 INSERT INTO MQ VALUES
-(0,"Siqi", "Matching the first name and last name","#Zhang##Li##Du##Cheng#",
-		"#Hao##Haoran##Wenxiao##Siqi#", 8, "#CommonSense##Stanford#", 0); 
+(0,"Siqi", "Matching the first name and last name","Zhang#Li#Du#Cheng",
+		"Hao#Haoran#Wenxiao#Siqi#", 8, "CommonSense#Stanford", 0); 
