cc5e4cab9c3aceca98624d5068edbc746090edac
Haoran Li <aimee@dn0a229345.sunet>
2013-12-02 13:42:17 -0800

    servlet added

---
+++ src/quiz_web/QuizSummaryPageServlet.java
@@ -0,0 +1,54 @@
+package quiz_web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import quiz_model.Quiz;
+
+/**
+ * Servlet implementation class QuizSummaryPageServlet
+ */
+@WebServlet("/QuizSummaryPageServlet")
+public class QuizSummaryPageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public QuizSummaryPageServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Quiz quiz = (Quiz) request.getSession().getAttribute("Quiz");
+		if (quiz.isOnePage()) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("QuizSinglePage.jsp");
+			dispatcher.forward(request, response);
+		} else if (!quiz.isImmediateCorrection()) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("QuizMultiplePage.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("QuizMultiplePageWithCorrection.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+}
